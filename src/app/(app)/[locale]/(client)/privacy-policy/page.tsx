2e2f9106be93ce7cf2b16f2adb0aa22a83acbff9
olegtkach <olegontkach101@gmail.com>
2024-08-12 08:33:05 +0300

    fix: build

--- src/app/(app)/[locale]/(client)/privacy-policy/page.tsx
+++ src/app/(app)/[locale]/(client)/privacy-policy/page.tsx
@@ -1,9 +1,5 @@
 'use client'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
 import MarkdownToHtml from '@/data/privacy-policy.mdx'
-import { MDXRemote } from 'next-mdx-remote'
-import { MDXProvider } from '@mdx-js/react'
 
 const Page = () => {
   return (
