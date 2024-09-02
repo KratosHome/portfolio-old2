7862528e7eeb6d8394e7d6e15c30f469255c2de3
olegtkach <olegontkach101@gmail.com>
2024-09-02 08:30:08 +0300

    feat: active dashboard project

--- src/components/client/community-dashboard/community-dashboard.tsx
+++ src/components/client/community-dashboard/community-dashboard.tsx
@@ -2,6 +2,7 @@
 import { FC } from 'react'
 import Link from 'next/link'
 import { useLocale } from 'use-intl'
+import { usePathname } from 'next/navigation'
 import './community-dashboard.scss'
 import { ButtonBeck } from '@/components/UI/button-beck/button-beck'
 
@@ -11,28 +12,33 @@ interface CommunityDashboardProps {
 
 export const CommunityDashboard: FC<CommunityDashboardProps> = ({ data }) => {
   const locale = useLocale()
+  const pathname = usePathname()
 
   return (
     <div className="flex flex-col justify-center">
       <ButtonBeck />
       <div className="text-right">Сhoose which section you want to look at</div>
       <div className="flex flex-wrap items-center justify-center">
-        {data.map((item: any, index: number) => (
-          <div
-            key={item.name}
-            className="flex cursor-pointer items-center text-[20px] lg:text-[64px]"
-          >
-            <Link
-              href={`/${locale}/${item.link}`}
-              className="community-dashboard-card group relative block overflow-hidden !rounded-full p-[24px] uppercase lg:p-[32px]"
+        {data.map((item: any, index: number) => {
+          const isActive = pathname === `/${locale}/${item.link}`
+
+          return (
+            <div
+              key={item.name}
+              className="flex cursor-pointer items-center text-[20px] lg:text-[64px]"
             >
-              <span className="duration-300 group-hover:text-[#0B66F5]">
-                {item.name}
-              </span>
-            </Link>
-            {index < data.length - 1 && <span className="block">/</span>}{' '}
-          </div>
-        ))}
+              <Link
+                href={`/${locale}/${item.link}`}
+                className={`community-dashboard-card group relative block overflow-hidden !rounded-full p-[24px] uppercase lg:p-[32px] ${isActive ? 'active' : ''}`}
+              >
+                <span className="duration-300 group-hover:text-[#0B66F5]">
+                  {item.name}
+                </span>
+              </Link>
+              {index < data.length - 1 && <span className="block">/</span>}{' '}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
