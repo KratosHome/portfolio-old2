df74e2f72cc09adbb3cf143a04de9f4e7f1a591f
olegtkach <olegontkach101@gmail.com>
2024-10-14 15:14:01 +0300

    find: bug scroll

--- src/app/(app)/[locale]/(client)/page.tsx
+++ src/app/(app)/[locale]/(client)/page.tsx
@@ -55,15 +55,15 @@ const Home: FC<PageProps> = async ({ params: { locale } }) => {
       <HomeSnippets locale={locale} />
       <Hero />
       <Services services={services} />
+      <Projects projects={projects} />
+      <Experience experience={experience} />
+      <Reviews data={dataReviewsSend} />
+      <Faq data={faq} />
     </>
   )
 }
 
 export default Home
 /*
-      <Services services={services} />
-      <Projects projects={projects} />
-      <Experience experience={experience} />
-      <Reviews data={dataReviewsSend} />
-      <Faq data={faq} />
+
  */
