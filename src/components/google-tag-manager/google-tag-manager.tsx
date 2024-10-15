e81c65c2bd6a4e9370794532cb0ba5a44597e89c
olegtkach <olegontkach101@gmail.com>
2024-10-15 11:37:29 +0300

    fix: GoogleTagManager

--- src/components/google-tag-manager/google-tag-manager.tsx
+++ src/components/google-tag-manager/google-tag-manager.tsx
@@ -4,13 +4,16 @@ export const GoogleTagManager = () => {
   const id = process.env.NEXT_GTM_ID
   return (
     <>
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-VH1Y00D2RY"
+      />
       <Script id="google-analytics">
         {`
                        window.dataLayer = window.dataLayer || [];
                        function gtag(){dataLayer.push(arguments);}
                        gtag('js', new Date());
-                       gtag('config', '${id}');
+                       gtag('config', 'G-VH1Y00D2RY');
                     `}
       </Script>
     </>
