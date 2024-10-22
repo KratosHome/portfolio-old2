7a1e58d1b30348dbebc3b070dd16fce488bb034d
olegtkach <olegontkach101@gmail.com>
2024-10-22 13:59:39 +0300

    search: SEO pr

--- src/app/(app)/[locale]/(client)/page.tsx
+++ src/app/(app)/[locale]/(client)/page.tsx
@@ -16,6 +16,7 @@ import HomeSnippets from '@/components/client/snippets/home-snippets'
 import { Projects } from '@/components/client/projects/projects'
 import dynamic from 'next/dynamic'
 import { Loader } from '@/components/UI/client/loader/loader'
+import { Services } from '@/components/client/services/services'
 
 const Hero = dynamic(() => import('@/components/client/hero/hero'), {
   loading: () => <Loader />,
@@ -56,9 +57,12 @@ const Home: FC<PageProps> = async ({ params: { locale } }) => {
 
   return (
     <>
-      <Suspense name={'Hero'} fallback={<Loader />}>
-        <Hero />
-      </Suspense>
+      <Services services={services} />
+      <Projects projects={projects} />
+      <Projects projects={projects} />
+      <Experience experience={experience} />
+      <Reviews data={dataReviewsSend} />
+      <Faq data={faq} />
     </>
   )
 }
