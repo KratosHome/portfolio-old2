6897e95daf88979c5407289230ed51d092842e6b
olegtkach <olegontkach101@gmail.com>
2025-01-09 08:58:06 +0200

    fix: types createProject

--- src/server/project/create-project.server.ts
+++ src/server/project/create-project.server.ts
@@ -2,27 +2,33 @@
 import { connectToDb } from '@/server/connectToDb'
 import { Project } from '@/server/project/project-scheme.server'
 
-export const createProject = async (id: any, adminId: string, data: any) => {
+export const createProject = async (
+  id: string,
+  adminId: string,
+  data: IProject,
+) => {
   try {
     await connectToDb()
 
-    data.teams = {
-      userId: id,
-      role: 'owner',
-    }
+    data.teams = [
+      {
+        userId: id,
+        role: 'owner',
+      },
+    ]
 
     const project = new Project(data)
     await project.save()
 
     const projectJSON = project.toObject()
     projectJSON._id = projectJSON._id.toString()
-    projectJSON.workPlan = projectJSON.workPlan.map((item: any) => ({
+    projectJSON.workPlan = projectJSON.workPlan.map((item: IProject) => ({
       ...item,
       _id: item._id.toString(),
     }))
 
     return { success: true, project: projectJSON }
   } catch (err) {
-    return { success: false, message: "Can't create project" }
+    return { success: false, message: err, project: [] }
   }
 }
